b5a0f53ecb0e0027e6dd0d608fb6c776c488c687
Sahil Ali <sahil@Sahils-MacBook-Air.local>
2022-06-10 20:15:45 +0530

    first commit

---
+++ src/components/SingleProduct.js
@@ -0,0 +1,53 @@
+import { Button, Card } from "react-bootstrap"
+import Rating from "./Rating";
+import {CartState} from "../context/Context";
+
+const SingleProduct = ({prod}) => {
+
+const {
+    state: { cart },
+    dispatch,
+} = CartState();
+
+    return (
+        <div className='products'>
+          <Card>
+              <Card.Img variant='top' src={prod.image} alt={prod.name} />
+              <Card.Body>
+                  <Card.Title>{prod.name}</Card.Title>
+                  <Card.Subtitle style={{paddingBottom: 10}}>
+                      <span>₹ {prod.price.split(".")[0]}</span>
+                      {prod.fastDelivery ? (
+                          <div>Fast Delivery</div>
+                      ) : (
+                          <div>4 Days Delivery</div>
+                    )}
+                    <Rating rating={prod.ratings} />
+                  </Card.Subtitle>
+                  {cart.some((p)=> p.id === prod.id)? (
+                      <Button onClick = {()=>{
+                          dispatch({
+                              type: "Remove_from_Cart",
+                              payload: prod,
+                          });
+                      }} variant='danger'>
+                      Remove from Cart
+                  </Button>
+                  ):(
+                      <Button onClick = {()=>{
+                          dispatch({
+                              type: "Add_to_Cart",
+                              payload: prod,
+                          });
+                      }} disabled={!prod.inStock}>
+                      {!prod.inStock ? "Out of Stock" : "Add to Cart"}
+                  </Button>)}
+                  
+                  
+              </Card.Body>
+          </Card>
+        </div>
+    )
+}
+
+export default SingleProduct;
